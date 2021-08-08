8a20861e082d0b2e964c7c31272f16ff9961267d
shrisrivastav <shrijitsrivastav@gmail.com>
2021-08-08 23:48:10 +0530

    8/8/21

--- httpmodule.js
+++ httpmodule.js
@@ -1,8 +1,11 @@
 const http = require("http");                               //requiring http module
 
 const server = http.createServer(function(req,res)          //creating server/localhost
-{
-    res.write("node js running");               //to print on webpage
+{   
+    res.writeHead(200,{"content-type":"text/html"});           //to change text content type
+                                                                //here it is set as HTML
+                                                                
+    res.write("<h1>node js running<h1>");               //to print on webpage
     res.end();                                  //compulsory everytime /it is ending point
 
 }).listen(3000,()=>console.log("server running on port 3000"));
